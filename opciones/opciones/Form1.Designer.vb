8f2577118fe9b0db88e6fd24c2297b33c2ca8a92
gisella513 <30634900+gisella513@users.noreply.github.com>
2017-09-30 19:41:57 -0500

    Agregar el button Limpiar

--- opciones/opciones/Form1.Designer.vb
+++ opciones/opciones/Form1.Designer.vb
@@ -28,11 +28,12 @@ Partial Class Form1
         Me.Button4 = New System.Windows.Forms.Button()
         Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(33, 31)
+        Me.Button1.Location = New System.Drawing.Point(33, 72)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
@@ -41,7 +42,7 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(258, 172)
+        Me.Button2.Location = New System.Drawing.Point(259, 156)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(75, 23)
         Me.Button2.TabIndex = 1
@@ -50,16 +51,16 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(147, 172)
+        Me.Button3.Location = New System.Drawing.Point(147, 217)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(73, 26)
         Me.Button3.TabIndex = 2
-        Me.Button3.Text = "Modificar"
+        Me.Button3.Text = "Limpiar"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(33, 172)
+        Me.Button4.Location = New System.Drawing.Point(33, 156)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 23)
         Me.Button4.TabIndex = 3
@@ -68,9 +69,9 @@ Partial Class Form1
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(147, 75)
+        Me.TextBox1.Location = New System.Drawing.Point(143, 75)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(86, 20)
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
         Me.TextBox1.TabIndex = 4
         '
         'TextBox2
@@ -80,11 +81,21 @@ Partial Class Form1
         Me.TextBox2.Size = New System.Drawing.Size(100, 20)
         Me.TextBox2.TabIndex = 5
         '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(147, 154)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(73, 26)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Modificar"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(384, 285)
+        Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.Button4)
@@ -103,4 +114,5 @@ Partial Class Form1
     Friend WithEvents Button4 As System.Windows.Forms.Button
     Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button5 As System.Windows.Forms.Button
 End Class
--- opciones/opciones/Form1.vb
+++ opciones/opciones/Form1.vb
@@ -2,4 +2,12 @@
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
 
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Sincronizacion de git hub actualizada en tiempo real();
+    End Sub
 End Class
